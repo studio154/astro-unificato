c0b545d947409858b5b8b997e0264670a5fb11c8
Elio Carchidi <studio154@studio154.it>
2026-05-30 14:39:58 +0200

    fix: clean progetti pages and resolve conflicts

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,12 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_ZaBraV from "/src/images/pacchetti/avanzato/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fesecutivo.md";
 import __ASTRO_IMAGE_IMPORT_cRQbU from "/src/images/pacchetti/base/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fbase.md";
-<<<<<<< HEAD
-import __ASTRO_IMAGE_IMPORT_u3Y0T from "/src/images/pacchetti/intermedio/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fintermedio.md";
-export default new Map([["/src/images/pacchetti/avanzato/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fesecutivo.md", __ASTRO_IMAGE_IMPORT_ZaBraV], ["/src/images/pacchetti/base/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fbase.md", __ASTRO_IMAGE_IMPORT_cRQbU], ["/src/images/pacchetti/intermedio/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fintermedio.md", __ASTRO_IMAGE_IMPORT_u3Y0T]]);
-=======
 import __ASTRO_IMAGE_IMPORT_ZaBraV from "/src/images/pacchetti/avanzato/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fesecutivo.md";
 import __ASTRO_IMAGE_IMPORT_u3Y0T from "/src/images/pacchetti/intermedio/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fintermedio.md";
 export default new Map([["/src/images/pacchetti/base/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fbase.md", __ASTRO_IMAGE_IMPORT_cRQbU], ["/src/images/pacchetti/avanzato/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fesecutivo.md", __ASTRO_IMAGE_IMPORT_ZaBraV], ["/src/images/pacchetti/intermedio/thumbnail.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fsessioni%2Fintermedio.md", __ASTRO_IMAGE_IMPORT_u3Y0T]]);
->>>>>>> a398e98 (nuovo layout per pagine, nuove classi per la struttura delle pagine)
 		
